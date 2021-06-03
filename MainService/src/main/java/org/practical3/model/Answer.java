0d1deb9f49b1db227bb74f79a2519d298e09a111
filon13p <filon13p@gmail.com>
2021-06-03 16:37:11 +0300

    + getPosts for main

---
+++ MainService/src/main/java/org/practical3/model/Answer.java
@@ -0,0 +1,13 @@
+package org.practical3.model;
+
+import java.util.Collection;
+
+public class Answer {
+    public Object Data;
+    public String Status;
+    public Answer(Object data, String status) {
+        Data = data;
+        Status = status;
+    }
+}
+
--- PostService/src/main/java/org/practical3/model/AnswerWall.java
+++
@@ -1,8 +0,0 @@
-package org.practical3.model;
-
-import java.util.Collection;
-
-public class AnswerWall {
-    public String Status;
-    public Collection<Post> Wall;
-}
